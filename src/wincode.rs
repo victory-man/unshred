8bc4cc0c5135eb56519d8866243dce2b3ef9aff1
victory-main <victory-main@protonmail.com>
2025-12-29 23:47:05 +0800

    fix

--- src/wincode.rs
+++ src/wincode.rs
@@ -306,7 +306,7 @@ impl Reader<'_> for SkipBytesReader {
     }
 
     unsafe fn consume_unchecked(&mut self, amt: usize) {
-        self.consume(amt).unwrap()
+        let _ = self.consume(amt);
     }
 
     fn consume(&mut self, amt: usize) -> wincode::io::ReadResult<()> {
@@ -322,9 +322,10 @@ impl Reader<'_> for SkipBytesReader {
                 self.offset += advance;
                 unadvanced -= advance;
                 // next chunk
-                if self.offset >= chunk.len() {
+                if self.offset == chunk.len() {
                     self.offset = 0;
                     self.chunk_idx += 1;
+                    iter = self.chunks.iter().skip(self.chunk_idx);
                 }
             } else {
                 return Err(wincode::io::ReadError::ReadSizeLimit(unadvanced));
